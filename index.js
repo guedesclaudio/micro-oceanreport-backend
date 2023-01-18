9c6fe68d24599ce3adbb71e61c7bbc5f2b90e41b
guedesclaudio <cgsarmentosilva@gmail.com>
2023-01-18 20:11:09 -0300

    feat: updated url

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const server = express();
 server
     .use(express.json())
 
-server.get("/waves", async (req, res) => {
+server.get("/waves/:date", async (req, res) => {
 
-    //const { date } = req.params;
+    const { date } = req.params;
 
-    //if (!date) return res.sendStatus(400);
+    if (!date) return res.sendStatus(400);
 
     try {
-        const url = "https://simcosta.furg.br/api/oceanic_data?boiaID=12&type=json&time1=1673924400&time2=1674097200&params=Hsig_Significant_Wave_Height_m";
+        const url = `https://simcosta.furg.br/api/oceanic_data?boiaID=12&type=json&time1=1673924400&time2=${date}&params=Hsig_Significant_Wave_Height_m,Mean_Wave_Direction_deg,TAvg,Average_Temperature_deg_C,Average_Temperature_C`;
         const promise = await axios.get(url);
         return res.status(200).send(promise.data);
     } catch (error) {
