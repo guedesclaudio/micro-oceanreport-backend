eec02def957a48a93ace2f9b7f90ddb1477fc368
guedesclaudio <cgsarmentosilva@gmail.com>
2023-01-19 10:45:54 -0300

    feat: created a atmosphere conditions route

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = express();
 server
     .use(express.json())
 
-server.get("/waves/:date", async (req, res) => {
+server.get("/ocean/:date", async (req, res) => {
 
     const { date } = req.params;
 
@@ -14,6 +14,21 @@ server.get("/waves/:date", async (req, res) => {
 
     try {
         const url = `https://simcosta.furg.br/api/oceanic_data?boiaID=12&type=json&time1=1673924400&time2=${date}&params=Hsig_Significant_Wave_Height_m,Mean_Wave_Direction_deg,TAvg,Average_Temperature_deg_C,Average_Temperature_C`;
+        const promise = await axios.get(url);
+        return res.status(200).send(promise.data);
+    } catch (error) {
+        console.error(error);
+    }
+})
+
+server.get("/atmosphere/:date", async (req, res) => {
+
+    const { date } = req.params;
+
+    if (!date) return res.sendStatus(400);
+
+    try {
+        const url = `https://simcosta.furg.br/api/metereo_data?boiaID=3&type=json&time1=1674010800&time2=${date}&params=Average_wind_direction_N,Average_wind_speed,Average_Air_Temperature`;
         const promise = await axios.get(url);
         return res.status(200).send(promise.data);
     } catch (error) {
