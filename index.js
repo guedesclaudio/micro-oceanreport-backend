7b89b1ac8ee3b9eb71bc7dfcb62c42d6253ea63d
Claudio Guedes <101193755+guedesclaudio@users.noreply.github.com>
2024-03-26 19:36:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ server.get("/atmosphere/:date", async (req, res) => {
     if (!date) return res.sendStatus(400);
 
     try {
-        const url = `https://simcosta.furg.br/api/metereo_data?boiaID=3&type=json&time1=1674010800&time2=${date}&params=Average_wind_direction_N,Average_wind_speed,Average_Air_Temperature`;
+        const url = `https://simcosta.furg.br/api/metereo_data?boiaID=12&type=json&time1=1674010800&time2=${date}&params=Average_wind_direction_N,Average_wind_speed,Average_Air_Temperature`;
         const promise = await axios.get(url);
         return res.status(200).send(promise.data);
     } catch (error) {
