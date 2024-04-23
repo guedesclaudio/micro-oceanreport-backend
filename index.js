099f439577d38cc3fe99796fe3a3c3646f3f0654
Claudio Silva <claudio.silva@funcionalcorp.com.br>
2024-04-23 18:34:13 -0300

    feat: updated data result

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ server.get("/ocean/:date", async (req, res) => {
     try {
         const url = `https://simcosta.furg.br/api/oceanic_data?boiaID=12&type=json&time1=1673924400&time2=${date}&params=Hsig_Significant_Wave_Height_m,Mean_Wave_Direction_deg,TAvg,Average_Temperature_deg_C,Average_Temperature_C`;
         const promise = await axios.get(url);
-        return res.status(200).send(promise.data);
+        const result = promise.data[promise.data.length - 1];
+        return res.status(200).send([result]);
     } catch (error) {
         console.error(error);
     }
@@ -30,10 +31,13 @@ server.get("/atmosphere/:date", async (req, res) => {
     try {
         const url = `https://simcosta.furg.br/api/metereo_data?boiaID=12&type=json&time1=1674010800&time2=${date}&params=Average_wind_direction_N,Average_wind_speed,Average_Air_Temperature`;
         const promise = await axios.get(url);
-        return res.status(200).send(promise.data);
+        const result = promise.data[promise.data.length - 1];
+        return res.status(200).send([result]);
     } catch (error) {
         console.error(error);
     }
 })
 
-server.listen(4001, () => console.log("Server listen on PORT 4001"));
+server.listen(4001, () => {
+    console.log("Server listen on PORT 4001");
+});
