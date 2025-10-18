2f65244ede823d14769cd7858c38daab5416b928
guedesclaudio <cgsarmentosilva@gmail.com>
2025-10-18 17:32:20 -0300

    added logs

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ server.post("/proxy", async (req, res) => {
       return res.status(400).json(message);
     }
 
-    const baseUrl = "http://212.85.14.153:30081";
+    const baseUrl = "http://191.252.217.133:30081";
     const url = `${baseUrl}${endpoint}`;
 
     const response = await axios({
